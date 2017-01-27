06154f2d66dcc1e3b15ae4c0506b00bbe2b55ec6
Christian Schulte zu Berge <christian.szb@in.tum.de>
2017-01-27 20:50:23 +0100

    Added utils::NotCopyable, utils::remove_erase_unique_ptr()

--- cts-core/include/cts-core/base/utils.h
+++ cts-core/include/cts-core/base/utils.h
@@ -7,6 +7,13 @@ namespace cts
 {
 	namespace utils
 	{
+		struct NotCopyable
+		{
+			NotCopyable() = default;
+			NotCopyable(const NotCopyable&) = delete;
+			NotCopyable& operator=(const NotCopyable&) = delete;
+		};
+
 
 		/// Convenience function implementing the remove-erase idiom to remove all elements from 
 		/// \e container that match \e value.
@@ -14,6 +21,15 @@ namespace cts
 		inline void remove_erase(Container& container, T&& value)
 		{
 			container.erase(std::remove(container.begin(), container.end(), std::forward<T>(value)), container.end());
+		}
+
+
+		/// Convenience function implementing the remove-erase idiom to remove all elements from 
+		/// \e container containing unique_ptrs that match \e value.
+		template<typename Container, typename T>
+		inline void remove_erase_unique_ptr(Container& container, T* value)
+		{
+			container.erase(std::remove_if(container.begin(), container.end(), [value](const std::unique_ptr<T>& ptr) { return ptr.get() == value; }), container.end());
 		}
 
 
